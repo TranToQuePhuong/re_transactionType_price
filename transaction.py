77f0d8e37824411d56701e1e3ca314aa8c257ba6
TranToQuePhuong <40876961+TranToQuePhuong@users.noreply.github.com>
2020-07-25 17:12:34 +0700

    Solve Transaction_type
    Continue solving price_sell, price_rent

--- transaction.py
+++ transaction.py
@@ -21,70 +21,84 @@ def to_str(data):
 
 
 def cal_price_rent(post_info, raw_price, x):
-    if x == '':
-        return raw_price * post_info.get_info('area_cal')
-    price = raw_price
-    rent_ptrn = params.val_ptrn + params.cur_u_r + params.time_u
-    if type(x) is not str:
-        txt = to_str(x[0])
-    else:
-        txt = x
-    if search(rent_ptrn, txt, I):
-        if search(r'\/\s*\d*\s*m\s*2', txt, I):
-            price *= post_info.get_info('area_cal')
-        if search(r'\/\s*1?\s*năm', txt, I):
-            price /= 12
-    return price
+    att = post_info['attributes']
+    for i in range(0, len(att)):
+        if att[i]['type'] == 'area':
+            if x == '':
+                return raw_price * att[i]['content']
+            price = raw_price
+            rent_ptrn = params.val_ptrn + params.cur_u_r + params.time_u
+            if type(x) is not str:
+                txt = to_str(x[0])
+            else:
+                txt = x
+            if search(rent_ptrn, txt, I):
+                if search(r'\/\s*\d*\s*m\s*2', txt, I):
+                    price *= att[i]['content']
+                if search(r'\/\s*1?\s*năm', txt, I):
+                    price /= 12
+            return price
 
 
 def get_price_rent(post_info, trans_type, attr_price_str):
     if trans_type in [2, 4, 5, 7]:
-        data = [(post_info.get_info('content'), 'message'),
-                (post_info.get_info('price_str'), 'price_str'),
-                (attr_price_str, 'price_str')]
-        for x in data:
-            price_rent = check_rent.extract_number_lst(x)
-            if price_rent[0]:
-                if len(price_rent) == 3:
-                    return cal_price_rent(post_info, price_rent[1], price_rent[2]) * 1.0
-                return cal_price_rent(post_info, price_rent[1], x) * 1.0
-        if type(post_info.get_info('price_m2')) in [float, int]:
-            tmp = float(post_info.get_info('price_m2'))
-            if tmp > 0.0:
-                return cal_price_rent(post_info, tmp, '') * 1.0
+        att = post_info['attributes']
+        for i in range(0, len(att)):
+            if att[i]['type'] == 'price':
+                data = [(post_info['content'], 'message'),
+                        (att[i]['type'], 'price'),
+                        (attr_price_str, 'price')]
+                for x in data:
+                    price_rent = check_rent.extract_number_lst(x)
+                    if price_rent[0]:
+                        if len(price_rent) == 3:
+                            return cal_price_rent(post_info, price_rent[1], price_rent[2]) * 1.0
+                        return cal_price_rent(post_info, price_rent[1], x) * 1.0
+                if att[i]['type'] == 'area':
+                    if type(att[i]['content']) in [float, int]:
+                        tmp = float(att[i]['content'])
+                        if tmp > 0.0:
+                            return cal_price_rent(post_info, tmp, '') * 1.0
     return 0.0
 
 
 def cal_price_sell(post_info, raw_price, x):
-    if x == '':
-        return raw_price * post_info.get_info('area_cal')
-    if x == 'f_price':
-        return raw_price
-    price = raw_price
-    sell_ptrn = params.val_ptrn_s + params.cur_u_s
-    txt = to_str(x[0])
-    if search(sell_ptrn, txt, I):
-        if search(params.u_pr_s_area, txt, I):
-            price *= post_info.get_info('area_cal')
-    return price
+    att = post_info['attributes']
+    for i in range(0, len(att)):
+        if att[i]['type'] == 'area':
+            if x == '':
+                return raw_price * att[i]['content']
+            if x == 'price':
+                return raw_price
+            price = raw_price
+            sell_ptrn = params.val_ptrn_s + params.cur_u_s
+            txt = to_str(x[0])
+            if search(sell_ptrn, txt, I):
+                if search(params.u_pr_s_area, txt, I):
+                    price *= att[i]['content']
+            return price
 
 
 def get_price_sell(post_info, trans_type, attr_price_str):
     if trans_type in [1, 3, 4, 5, 6, 7]:
-        data = [(post_info.get_info('content'), 'message'),
-                (post_info.get_info('price_str'), 'price_str'),
-                (attr_price_str, 'price_str')]
-        for x in data:
-            price_sell=check_sell.extract_number_lst(x)
-            if price_sell[0]:
-                if len(price_sell) == 3:
-                    return cal_price_sell(post_info, price_sell[1], price_sell[2]) * 1.0
-                return cal_price_sell(post_info, price_sell[1], x) * 1.0
-        if trans_type != 4:
-            if type(post_info.get_info('price_m2')) in [float, int]:
-                tmp = float(post_info.get_info('price_m2'))
-                if tmp > 0.0:
-                    return cal_price_sell(post_info, tmp, '') * 1.0
+        att = post_info['attributes']
+        for i in range(0, len(att)):
+            if att[i]['type'] == 'price':
+                data = [(post_info['content'], 'message'),
+                    (att[i]['content'], 'price'),
+                    (attr_price_str, 'price')]
+                for x in data:
+                    price_sell=check_sell.extract_number_lst(x)
+                    if price_sell[0]:
+                        if len(price_sell) == 3:
+                            return cal_price_sell(post_info, price_sell[1], price_sell[2]) * 1.0
+                        return cal_price_sell(post_info, price_sell[1], x) * 1.0
+                if trans_type != 4:
+                    if att[i]['type'] =='area':
+                        if type(att[i]['type']) in [float, int]:
+                            tmp = float(att[i]['type'])
+                        if tmp > 0.0:
+                            return cal_price_sell(post_info, tmp, '') * 1.0
     return 0.0
 
 
@@ -93,30 +107,34 @@ def get_trans_type(post_info):
     """Phân giải trường `transaction_type` một cách phù hợp
     """
 
-    data = [(post_info.get_info('content'), 'message'),
-            (post_info.get_info('price_str'), 'price_str')]
-    if post_info.get_info('transaction_type'):
-        data.append((post_info.get_info('transaction_type'), 'transaction_type'))
-        return filter_post(data)
-    """
-    Options:
-        1 - Post chỉ bán
-        2 - Post chỉ thuê
-        3 - Post sang nhượng
-        4 - Post vừa bán vừa thuê
-        5 - Post bán, đang cho thuê, có giá thuê
-        6 - Post bán, đang cho thuê, không có giá thuê
-        7 - Khác
-    """
-    for x in [4, 5, 6, 2, 1, 3, 7]:
-        if x == 2:
-            data.append(('thuê', 'transaction_type'))
-        if x in [5, 6]:
-            if filter_post_option(data=data, option=1):
-                if filter_post_option(data=data, option=x):
+    att = post_info['attributes']
+    for i in range(0,len(att)):
+        if att[i]['type'] == 'price':
+            data = [(post_info['content'],'message'),
+                 (att[i]['type'],'price')]
+            if att[i]['type']=='transaction_type':
+                data.append((att[i]['type'], 'transaction_type'))
+                return filter_post(data)
+            """
+            Options:
+                1 - Post chỉ bán
+                2 - Post chỉ thuê
+                3 - Post sang nhượng
+                4 - Post vừa bán vừa thuê
+                5 - Post bán, đang cho thuê, có giá thuê
+                6 - Post bán, đang cho thuê, không có giá thuê
+                7 - Khác
+            """
+            for x in [4, 5, 6, 2, 1, 3, 7]:
+                if x == 2:
+                    data.append(('thuê', 'transaction_type'))
+                if x in [5, 6]:
+                    if filter_post_option(data=data, option=1):
+                        if filter_post_option(data=data, option=x):
+                            return x
+                elif filter_post_option(data=data, option=x):
                     return x
-        elif filter_post_option(data=data, option=x):
-            return x
+    return 7
 
 
 def filter_post(data):
@@ -131,19 +149,20 @@ def filter_post(data):
     '''
     if sell_and_rent.get_data(data):
         return 4
-    if s_r_w_pr.get_data(data):
+    elif s_r_w_pr.get_data(data):
         return 5
-    if s_r_wo_pr.get_data(data):
+    elif s_r_wo_pr.get_data(data):
         return 6
-    if check_rent.get_data(data):
+    elif check_rent.get_data(data):
         return 2
-    if check_sell.get_data(data):
+    elif check_sell.get_data(data):
         return 1
-    if transfer.get_data(data):
+    elif transfer.get_data(data):
         return 3
     # if s_w_b_p.get_data(data):
     #     return "Post bán và có tiềm năng kinh doanh"
-    return 7
+    else:
+        return 7
 
 
 def filter_post_option(data, option):
@@ -174,9 +193,46 @@ def filter_post_option(data, option):
 
 with open('data.json', encoding="utf-8") as json_file:
     dataset = json.load(json_file)
+    count_1=0
+    count_2=0
+    count_3=0
+    count_4=0
+    count_5=0
+    count_6=0
+    count_7=0
     for data in dataset:
-        post_info = PostInfo()
-        print (get_trans_type(post_info))
+        print(data['id'])
+        print ("Transaction_type: ", get_trans_type(data))
+        # att = data['attributes']
+        # for i in range(0, len(att)):
+        #   if att[i]['type'] == 'price':
+        #      print("Price_sell: ", get_price_sell(data,get_trans_type(data),att[i]['content']))
+        #      print("Price_rent: ", get_price_rent(data,get_trans_type(data),att[i]['content']))
+        if get_trans_type(data)==1:
+            count_1 = count_1 + 1
+        elif get_trans_type(data)==2:
+            count_2 = count_2 + 1
+        elif get_trans_type(data)==3:
+            count_3 = count_3 + 1
+        elif get_trans_type(data)==4:
+            count_4 = count_4 + 1
+        elif get_trans_type(data)==5:
+            count_5 = count_5 + 1
+        elif get_trans_type(data)==6:
+            count_6 = count_6 + 1
+        elif get_trans_type(data)==7:
+            count_7 = count_7 + 1
+        print("\n")
+        
+    print(count_1)
+    print(count_2)
+    print(count_3)
+    print(count_4)
+    print(count_5)
+    print(count_6)
+    print(count_7)
+
+       # print ("Price_sell: ", get_price_sell(data,get_trans_type(data), price))
 
 
 
